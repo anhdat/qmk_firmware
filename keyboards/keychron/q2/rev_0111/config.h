22c0a529d4970ef625c86f54506f95f6b6b2d496
datadog compute robot <datadog-compute-robot@datadoghq.com>
2022-03-14 07:47:32 +0100

    Dat Keychron Q2 with homerow mod

--- keyboards/keychron/q2/rev_0111/config.h
+++ keyboards/keychron/q2/rev_0111/config.h
@@ -31,3 +31,12 @@
 
 /* Specifies the number of pulses the encoder registers between each detent */
 #define ENCODER_RESOLUTION 4
+
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
