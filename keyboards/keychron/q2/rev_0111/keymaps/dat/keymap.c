f0a956b1e4e4055602197ef57d264efc0d381648
datadog compute robot <datadog-compute-robot@datadoghq.com>
2022-04-07 11:45:24 +0200

    Format keymap

--- keyboards/keychron/q2/rev_0111/keymaps/dat/keymap.c
+++ keyboards/keychron/q2/rev_0111/keymaps/dat/keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2021 @ Keychron (https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include QMK_KEYBOARD_H
 
@@ -40,33 +25,36 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
-        KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,       KC_0,        KC_MINS,  KC_EQL,   KC_BSLS,          KC_MUTE,
-        KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,        KC_BSPC,  KC_LBRC,  KC_RBRC,            KC_MPLY,
-        CT(KC_ESC),    CT(KC_A),AT(KC_S),ST(KC_D),GT(KC_F),KC_G,    KC_H,    GT(KC_J),ST(KC_K),AT(KC_L),   OSL(_SYM),   KC_QUOT,            KC_ENT,           KC_MNXT,
-        KC_LSFT,       KC_Z,    GT(KC_X),KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, GT(KC_DOT), KC_SLSH,     KC_RSFT,  KC_UP,
-        KC_LCTL,       KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, MO(_FN1), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT
+KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,       KC_0,      KC_MINS,  KC_EQL,   KC_BSLS,  KC_MUTE,  
+KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,       KC_P,      KC_BSPC,  KC_LBRC,  KC_RBRC,  KC_MPLY,  
+CT(KC_ESC), CT(KC_A), AT(KC_S), ST(KC_D), GT(KC_F), KC_G,     KC_H,     GT(KC_J), ST(KC_K), AT(KC_L),   OSL(_SYM), KC_QUOT,  KC_ENT,             KC_MNXT,  
+KC_LSFT,    KC_Z,     GT(KC_X), KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  GT(KC_DOT), KC_SLSH,   KC_RSFT,            KC_UP,    
+KC_LCTL,    KC_LOPT,  KC_LCMD,                      KC_SPC,                       KC_RCMD,  MO(_FN1),   MO(_FN3),            KC_LEFT,  KC_DOWN,  KC_RGHT   
     ),
 
     [_SYM] = LAYOUT_ansi_67(
-        KC_GRV,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,    KC_VOLD,  KC_VOLU,   _______,          _______,
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______,    _______,  _______,   _______,          _______,
-        _______, S(KC_EQL), KC_MINS, KC_LCBR, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_RCBR, KC_SCLN, S(KC_SCLN), _______,             _______,          _______,
-        _______, KC_EQL,  S(KC_MINS), S(KC_BSLS), _______, _______, _______, _______, _______, _______,    _______,             _______, _______,
-        _______, _______, _______,                            _______,                            _______, _______,  _______,   _______, _______, _______),
+KC_GRV,   KC_BRID,   KC_BRIU,    KC_MCTL,    KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,    KC_VOLD,  KC_VOLU,  _______,  _______,  
+KC_TILD,  KC_EXLM,   KC_AT,      KC_HASH,    KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  _______,  _______,    _______,  _______,  _______,  _______,  
+_______,  S(KC_EQL), KC_MINS,    KC_LCBR,    KC_LPRN,  KC_LBRC,  KC_RBRC,  KC_RPRN,  KC_RCBR,  KC_SCLN,  S(KC_SCLN), _______,  _______,            _______,  
+_______,  KC_EQL,    S(KC_MINS), S(KC_BSLS), _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,            _______,  
+_______,  _______,   _______,                          _______,                      _______,  _______,  _______,              _______,  _______,  _______   
+        ),
 
     [_FN1] = LAYOUT_ansi_67(
-        KC_GRV,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,   _______,          _______,
-        RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,   _______,          _______,
-        _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,             _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______,
-        _______, _______, _______,                            _______,                            _______, _______,  _______,   _______, _______, _______),
+KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  
+RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  
+_______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  
+_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  
+_______,  _______,  _______,                      _______,                      _______,  _______,  _______,            _______,  _______,  _______   
+        ),
 
     [_FN3] = LAYOUT_ansi_67(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    _______,          _______,
-        RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,   _______,          _______,
-        _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,             _______,          RESET,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______,
-        _______, _______, _______,                            _______,                            _______, _______,  _______,   _______, _______, _______)
+KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  
+RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  
+_______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            RESET,    
+_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  
+_______,  _______,  _______,                      _______,                      _______,  _______,  _______,            _______,  _______,  _______   
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
