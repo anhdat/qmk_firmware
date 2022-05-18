dd474fc13c912e1d77d299412c5b2af2d0c18f1d
Dat Truong <mr.anhdat@gmail.com>
2022-05-18 09:25:40 +0200

    Add dat xd75

--- keyboards/xiudi/xd75/config.h
+++ keyboards/xiudi/xd75/config.h
@@ -140,3 +140,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
