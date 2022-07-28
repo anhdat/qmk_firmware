67b2d2c4ad570810e95bffedeafe1fa392da3854
Dat Truong <mr.anhdat@gmail.com>
2022-07-28 11:15:38 +0200

    Enable matrix rgb, change some symbols, disable mouse key to save mem

--- keyboards/keychron/v4/config.h
+++ keyboards/keychron/v4/config.h
@@ -40,3 +40,6 @@
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
+
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
